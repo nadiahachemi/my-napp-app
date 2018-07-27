c2afc190ee880bb69707196d0d6be590376a3916
Nadia Hachemi <nadia_hachemi@live.fr>
2018-07-27 15:51:23 +0200

    almost done I swear

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "../../../node_modules/@angular/router";
+
 
 @Component({
   selector: 'app-chart',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+  constructor( public myRouterServ: Router) { }
 
   ngOnInit() {
   }
